84959af5bb61b10df13129fa2788ee3f83e09ae8
SH <SH@example.com>
2020-03-25 13:23:08 +0900

    Add recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+# サンプルユーザー作成
 def sample_user(email='test@example.com', password='test123'):
     """Create a sample user"""
     return get_user_model().objects.create_user(email, password)
@@ -11,6 +12,7 @@ def sample_user(email='test@example.com', password='test123'):
 
 class ModelTests(TestCase):
 
+    # ユーザーのメアドとパスワードチェック
     def test_create_user_save_email_and_password(self):
         """Test creating a new user with an email is successful"""
         email = 'test@example.com'
@@ -22,17 +24,20 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
+    # メアドが小文字になるか
     def test_create_user_normalize_email(self):
         """Test the email for a new user is normalized"""
         email = 'test@EXAMPLE.COM'
         user = get_user_model().objects.create_user(email, 'test123')
         self.assertEqual(user.email, email.lower())
 
+    # メアドがないときエラーになるか
     def test_create_user_not_email_raise(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
 
+    # adminユーザーが作成できるか
     def test_create_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
@@ -60,3 +65,13 @@ class ModelTests(TestCase):
             name='Cucumber'
         )
         self.assertEqual(str(ingredient), ingredient.name)
+
+    # recipeが生成できるか
+    def test_recipe_str(self):
+        """Test the recipe string respresentation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            time_minutes=5,
+            price=5.00
+        )
+        self.assertEqual(str(recipe), recipe.title)
