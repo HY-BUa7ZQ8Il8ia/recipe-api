d36ac3d731a399dd8b5a9bd01a71b050e75011b4
SH <SH@example.com>
2020-04-02 13:02:16 +0900

    Add file_path and modify recipe models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
+from unittest.mock import patch
+
 from django.contrib.auth import get_user_model
+from django.test import TestCase
 
 from core import models
 
@@ -75,3 +77,14 @@ class ModelTests(TestCase):
             price=5.00
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    # imageをパス通りに保存できるか
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test that image is saved in the correct location"""
+        uuid = 'test-id'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
